c32520308db0700daa00385eacd4e9ef3338c91b
Tim Scully <tim.scully@gblo-cs03676.omega.wwads.net>
2015-02-12 12:21:06 +0000

    got elements by id

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+var Banner = function() {
+
+  function get(element) {
+    return document.getElementById(element);
+  }
+
+  var container = get('container'),
+      common = get('common'),
+      elipse1 = get('frame1_elipse'),
+      text1 = get('text1'),
+      elipse2 = get('elipse2'),
+      text2 = get('text2'),
+      elipse3 = get('frame3_elipse'),
+      conditions = get('conditions');
+
+
+};
